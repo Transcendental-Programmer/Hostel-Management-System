264c6a9e17cfb132568ac28cfc348a997498ed52
Shreshtha-Garg <raghavgarg037@gmail.com>
2024-10-27 00:16:45 +0530

    updated users schema by diving it into 3 tables namely users, admins and staffs. Also updated the userController accordingly.

--- backend/models/admin.js
+++ backend/models/admin.js
@@ -3,29 +3,24 @@ import { v4 as uuidv4 } from 'uuid';
 
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
+const AdminSchema = new Schema({
     user_id: { type: String, default: uuidv4, unique: true },
     username: { type: String, unique: true, required: true, maxlength: 50 },
     password_hash: { type: String, required: true, maxlength: 255 },
     email: { type: String, unique: true, required: true, maxlength: 100 },
     full_name: { type: String, required: true, maxlength: 100 },
     phone_number: { type: String, maxlength: 15, default: null },
-    // role: { type: String, required: true },
-    // department_id: { type: String, default: null },
-    language_preference: { type: String, default: "English", maxlength: 20 },
-    roll_number: { type: String, maxlength: 20,required: true, default: null },
     hostel_number: { type: String, maxlength: 20, default: null },
-    room_number: { type: String, maxlength: 20, default: null },
-    floor_number: { type: Number, default: null },
+    language_preference: { type: String, default: "English", maxlength: 20 },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
     is_active: { type: Boolean, default: true },
-    available_time_slot: { type: String, maxlength: 50, default: null },
 });
 
-UserSchema.pre('save', function (next) {
+// Update timestamp before saving
+AdminSchema.pre('save', function (next) {
     this.updated_at = Date.now();
     next();
 });
 
-export default mongoose.model('User', UserSchema);
+export default mongoose.model('Admin', AdminSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import { registerUser, userLogin, verifyOtpAndRegister,createWorker } from '../controller/userController.js';
+import { registerUser, userLogin, verifyOtpAndRegister,createStaff,
+    updateUserDetails, getUserDetailsById
+ } from '../controller/userController.js';
 
 const userRoutes = express.Router();
 
 userRoutes.post("/register", registerUser);
 userRoutes.post("/login", userLogin);
 userRoutes.post("/verify-otp", verifyOtpAndRegister);
-userRoutes.post("/createWorker", createWorker);
+userRoutes.post("/createStaff", createStaff);
+userRoutes.put("/updateUserDetails/:user_id", updateUserDetails);
+userRoutes.get("/getUserDetailsById/:user_id", getUserDetailsById);
 
 export default userRoutes;
