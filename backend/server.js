3cee17f0992a00b80ee45117adb115a31d5ea5f9
Shreshtha-Garg <raghavgarg037@gmail.com>
2024-10-27 23:11:29 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import MongoDB from './config/MongoDB.js';
+import client from './config/redisClient.js';
 import SocketService from './config/socket.js';
-import grievanceRoutes from './routes/grievanceRoutes.js';
+// import complaintRoutes from './routes/complaintRoutes.js';
+// import studentRoutes from './routes/studentRoutes.js';
+// import wardenRoutes from './routes/wardenRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import grievanceRoutes from './routes/grievanceRoutes.js';
+import http from 'http';
+
 
 const app = express();
 MongoDB();
@@ -11,13 +17,22 @@ MongoDB();
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/", grievanceRoutes);
+// app.use("/", studentRoutes);
+// app.use("/", wardenRoutes);
 app.use("/users/", userRoutes);
 
-const PORT = 3000;
-const server = app.listen(PORT, () => {
-  console.log(`Application is running on port ${PORT}`);
+// Initialize the Redis connection
+await client.connect(
+  console.log('Connected to Redis')
+);
+client.on('error', (err) => console.error('Redis Client Error', err));
+
+// Create the HTTP server using the Express app
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log("Application is running on port 3000");
 });
 
 // Initialize Socket.io
