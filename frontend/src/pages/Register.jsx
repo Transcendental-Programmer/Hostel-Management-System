36d91a36c300ef4940c1c655d22999f9d2ecccec
YASHRATHORE2004 <yashrathore2004.yr@gmail.com>
2024-11-06 19:59:32 +0530

    updated auth ui

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -165,98 +165,33 @@ function Register() {
 
   return (
     <>
-      <div className="flex min-h-screen w-screen items-center justify-center text-gray-600 bg-gray-50">
-        <div className="relative">
-          <div className="hidden sm:block h-56 w-56 text-indigo-300 absolute a-z-10 -left-20 -top-20">
-            <svg
-              id="patternId"
-              width="100%"
-              height="100%"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <defs>
-                <pattern
-                  id="a"
-                  patternUnits="userSpaceOnUse"
-                  width="40"
-                  height="40"
-                  patternTransform="scale(0.6) rotate(0)"
-                >
-                  <rect x="0" y="0" width="100%" height="100%" fill="none" />
-                  <path
-                    d="M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5"
-                    stroke-width="1"
-                    stroke="none"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width="800%"
-                height="800%"
-                transform="translate(0,0)"
-                fill="url(#a)"
-              />
-            </svg>
-          </div>
-          <div className="hidden sm:block h-28 w-28 text-indigo-300 absolute a-z-10 -right-20 -bottom-20">
-            <svg
-              id="patternId"
-              width="100%"
-              height="100%"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <defs>
-                <pattern
-                  id="b"
-                  patternUnits="userSpaceOnUse"
-                  width="40"
-                  height="40"
-                  patternTransform="scale(0.5) rotate(0)"
-                >
-                  <rect x="0" y="0" width="100%" height="100%" fill="none" />
-                  <path
-                    d="M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5"
-                    stroke-width="1"
-                    stroke="none"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width="800%"
-                height="800%"
-                transform="translate(0,0)"
-                fill="url(#b)"
-              />
-            </svg>
-          </div>
-
-          <div className="relative flex flex-col sm:w-full md:w-[30rem] lg:w-[30rem] xl:w-[30rem] 2xl:w-[30rem] rounded-lg border-gray-400 bg-white shadow-lg px-4">
-            <div className="flex-auto p-6">
-              <div className="mb-10 flex flex-shrink-0 flex-grow-0 items-center justify-center overflow-hidden">
+      <div className="flex min-h-screen items-center justify-center text-gray-600">
+        <div className="relative my-5">
+          <div className="h-full relative flex flex-col sm:w-full md:w-[30rem] lg:w-[30rem] xl:w-[30rem] 2xl:w-[30rem] rounded-lg border-gray-400 bg-indigo-200 shadow-lg px-4">
+            <div className="flex-auto h-full p-4">
+              <div className="mb-3 flex flex-shrink-0 flex-grow-0 items-center justify-center overflow-hidden">
                 <a
                   href="#"
                   className="flex cursor-pointer items-center gap-2 text-indigo-500 no-underline hover:text-indigo-500"
                 >
                   <span className="flex-shrink-0 text-3xl font-black tracking-tight opacity-100">
-                    Signup.
+                    Signup
                   </span>
                 </a>
               </div>
 
-              <h4 className="mb-2 font-medium text-gray-700 xl:text-xl">
+              <h4 className="mb-1 font-medium text-gray-700 xl:text-xl">
                 Welcome!
               </h4>
-              <p className="mb-6 text-gray-500">
+              <p className="mb-3 text-gray-500">
                 Please fill in your details to create an account
               </p>
 
               <form className="mb-4" action="#" method="POST">
-                <div className="mb-4">
+                <div className="mb-1">
                   <div className="flex justify-between">
                     <label
-                      className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
+                      className="mb-1 inline-block text-xs font-medium uppercase text-gray-700"
                       htmlFor="fullname"
                     >
                       Full Name
@@ -277,7 +212,7 @@ function Register() {
                   <div className="flex-1">
                     <label
                       htmlFor="email"
-                      className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
+                      className="mb-1 inline-block text-xs font-medium uppercase text-gray-700"
                     >
                       Email
                     </label>
@@ -296,7 +231,7 @@ function Register() {
                   <div className="flex-1">
                     <label
                       htmlFor="phone"
-                      className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
+                      className="mb-1 inline-block text-xs font-medium uppercase text-gray-700"
                     >
                       Phone Number
                     </label>
@@ -316,7 +251,7 @@ function Register() {
                   <div className={role === Roles.STUDENT ? "flex-1" : "w-full"}>
                     <label
                       htmlFor="username"
-                      className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
+                      className="mb-1 inline-block text-xs font-medium uppercase text-gray-700"
                     >
                       Username
                     </label>
@@ -336,7 +271,7 @@ function Register() {
                     <div className="flex-1">
                       <label
                         htmlFor="roll_number"
-                        className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
+                        className="mb-1 inline-block text-xs font-medium uppercase text-gray-700"
                       >
                         Roll no.
                       </label>
@@ -356,7 +291,7 @@ function Register() {
                   <div className="flex-1">
                     <label
                       htmlFor="hostel_number"
-                      className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
+                      className="mb-1 inline-block text-xs font-medium uppercase text-gray-700"
                     >
                       hostel_number
                     </label>
@@ -374,7 +309,7 @@ function Register() {
                     <div className="flex-1">
                       <label
                         htmlFor="room"
-                        className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
+                        className="mb-1 inline-block text-xs font-medium uppercase text-gray-700"
                       >
                         Room
                       </label>
@@ -395,7 +330,7 @@ function Register() {
                   >
                     <label
                       htmlFor="password"
-                      className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
+                      className="mb-1 inline-block text-xs font-medium uppercase text-gray-700"
                     >
                       Password
                     </label>
@@ -414,7 +349,7 @@ function Register() {
                   <div className="flex-1">
                     <label
                       htmlFor="role"
-                      className="mb-2 inline-block text-xs font-medium uppercase text-gray-700"
+                      className="mb-1 inline-block text-xs font-medium uppercase text-gray-700"
                     >
                       Role
                     </label>
