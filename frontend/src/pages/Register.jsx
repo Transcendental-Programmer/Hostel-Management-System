c2ac0382a7ccb3245fa052c45c28b0e12bc3a416
AayushDhakad <dkdayush@gmail.com>
2024-11-08 01:47:01 +0530

    check added

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -77,9 +77,19 @@ function Register() {
     }
   };
 
+  const allowedEmails = ['imt_2022089@iiitm.ac.in', 'imt_2022120@iiittm.ac.in'];
+
   const onSubmit = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
+
+    const email = document.getElementById("email").value;
+    const role = document.getElementById("role").value;
+
+    if (role === "warden" && !allowedEmails.includes(email)) {
+      toast.error("Only specific emails are allowed for the warden role.");
+        return;
+    }
 
     try {
       if (!validateEmail(email)) {
@@ -433,7 +443,7 @@ function Register() {
                     type="submit"
                     onClick={onSubmit}
                   >
-                    Sign in
+                    Sign up
                   </button>
                 </div>
               </form>
