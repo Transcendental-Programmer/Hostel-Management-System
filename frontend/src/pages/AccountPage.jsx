b27a1e40c4998fb7efb93d446d77bcb7dd7a11b6
AayushDhakad <dkdayush@gmail.com>
2024-11-08 09:49:16 +0530

    conditional rendering

--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -14,10 +14,7 @@ function AccountPage() {
   const [userHostel, setHostel] = useState("");
   const [userType, setUserType] = useState(null);
 
- 
-
   useEffect(() => {
-   
     const storedUserDetails = JSON.parse(localStorage.getItem("user"));
     if (storedUserDetails) {
       setUserName(storedUserDetails.full_name || "");
@@ -29,7 +26,6 @@ function AccountPage() {
       setHostel(storedUserDetails.hostel_number || "");
     }
 
-   
     const storedUserType = localStorage.getItem("user_role");
     if (storedUserType) {
       setUserType(storedUserType); 
@@ -42,42 +38,48 @@ function AccountPage() {
       <h2 className="mt-20 ml-5 mr-5 text-2xl font-semibold">Profile</h2>
 
       <ul className="mt-6 flex flex-col ml-5 mr-5 ">
-        <li className="lg:w-1/3  sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
+        <li className="lg:w-1/3 sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
           <div className="flex items-center justify-between w-full">
             <span>Name</span>
             <span>{userName}</span>
           </div>
         </li>
-        <li className="lg:w-1/3  sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
+        <li className="lg:w-1/3 sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
           <div className="flex items-center justify-between w-full">
             <span>Email</span>
             <span>{useremail}</span>
           </div>
         </li>
-        <li className="lg:w-1/3  sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
+        <li className="lg:w-1/3 sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
           <div className="flex items-center justify-between w-full">
             <span>Phone</span>
             <span>{userphone}</span>
           </div>
         </li>
-        {userType !== "warden" && (
+        
+        {(userType === "student" || userType === "warden" || userType === "staff" || userType === "admin") && (
           <>
-            <li className="lg:w-1/3  sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
+            <li className="lg:w-1/3 sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
               <div className="flex items-center justify-between w-full">
                 <span>Username</span>
                 <span>{userUsername}</span>
               </div>
             </li>
-            <li className="lg:w-1/3  sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
-              <div className="flex items-center justify-between w-full">
-                <span>Roll no.</span>
-                <span>{userRollno}</span>
-              </div>
-            </li>
-            <li className="lg:w-1/3  sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
+            <li className="lg:w-1/3 sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
               <div className="flex items-center justify-between w-full">
                 <span>Hostel</span>
                 <span>{userHostel}</span>
+              </div>
+            </li>
+          </>
+        )}
+
+        {userType === "student" && (
+          <>
+            <li className="lg:w-1/3 sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
+              <div className="flex items-center justify-between w-full">
+                <span>Roll no.</span>
+                <span>{userRollno}</span>
               </div>
             </li>
             <li className="lg:w-1/3 sm:w-full inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
@@ -89,9 +91,9 @@ function AccountPage() {
           </>
         )}
       </ul>
-      <button class="mt-5 ml-5 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+      <button className="mt-5 ml-5 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
         <Link
-          class=" relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-blue-500 rounded-md group-hover:bg-opacity-0"
+          className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-blue-500 rounded-md group-hover:bg-opacity-0"
           to="/"
         >
           Back
