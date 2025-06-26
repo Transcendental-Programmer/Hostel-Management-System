8f8a8e127b8275a87d6430c1161ec8c6a4822088
Shreshtha-Garg <raghavgarg037@gmail.com>
2025-06-26 19:22:21 +0530

    updated redis conf

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -3,10 +3,10 @@ import { createClient } from 'redis';
 import 'dotenv/config';
 const client = createClient({
   socket: {
-    host: process.env.REDIS_HOST || 'localhost',
+    host: process.env.REDIS_HOST || 'redis-16224.crce206.ap-south-1-1.ec2.redns.redis-cloud.com',
     port: process.env.REDIS_PORT || 16224,
   },
-  password: process.env.REDIS_PASSWORD || undefined,
+  password: process.env.REDIS_PASSWORD || 'YnXNtPfK8mRUB3PYe1U4BPRit2XuMQQ5',
 });
 
 client.on('error', (err) => console.error('Redis Error:', err));
