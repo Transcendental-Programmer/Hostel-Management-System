5c3390c34e1f8e5de889a8428fac0074ba9b6fe4
Shreshtha-Garg <raghavgarg037@gmail.com>
2025-08-06 20:35:28 +0530

    updated files

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -3,10 +3,10 @@ import { createClient } from 'redis';
 import 'dotenv/config';
 const client = createClient({
   socket: {
-    host: process.env.REDIS_HOST || 'redis-16224.crce206.ap-south-1-1.ec2.redns.redis-cloud.com',
-    port: process.env.REDIS_PORT || 16224,
+    host: process.env.REDIS_HOST ,
+    port: process.env.REDIS_PORT ,
   },
-  password: process.env.REDIS_PASSWORD || 'YnXNtPfK8mRUB3PYe1U4BPRit2XuMQQ5',
+  password: process.env.REDIS_PASSWORD ,
 });
 
 client.on('error', (err) => console.error('Redis Error:', err));
--- backend/keepAlive.js
+++ backend/keepAlive.js
@@ -1,22 +1,47 @@
+// keepAlive.js
 import { createClient } from 'redis';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const client = createClient({ url: process.env.REDIS_URL });
+const pingClient = createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  password: process.env.REDIS_PASSWORD,
+});
 
-async function sendHeartbeat() {
+pingClient.on('error', (err) => console.error('Redis PingClient Error:', err));
+pingClient.on('connect', () => console.log('Redis PingClient Connected'));
+
+async function sendHeartbeats() {
+  // Redis
   try {
-    if (!client.isOpen) await client.connect();
-    await client.set('heartbeat', Date.now().toString());
+    if (!pingClient.isOpen) await pingClient.connect();
+    await pingClient.set('heartbeat', Date.now().toString());
     console.log('✅ Redis heartbeat set');
   } catch (err) {
     console.error('❌ Redis heartbeat error:', err);
   }
+
+  // MongoDB
+  try {
+    if (mongoose.connection.readyState !== 1) {
+      await mongoose.connect(process.env.MONGO_URI, {
+        dbName: 'test',
+      });
+    }
+    // console.log('MongoDB connected:', mongoose.connection.name);
+    if (!mongoose.connection.db) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+    const result = await mongoose.connection.db.command({ ping: 1 });
+    console.log(`✅ MongoDB ping result: ${result.ok}`);
+  } catch (err) {
+    console.error('❌ MongoDB heartbeat error:', err);
+  }
 }
 
-// First call immediately
-sendHeartbeat();
-
-// Then every 24 hours
-setInterval(sendHeartbeat, 24 * 60 * 60 * 1000);
+sendHeartbeats();
+setInterval(sendHeartbeats, 24 * 60 * 60 * 1000);
